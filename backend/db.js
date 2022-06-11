57ac76038c85a6dd1f59b05ce6538f5a265bf7c5
vardaan3012 <vverma_be19@thapar.edu>
2022-06-11 13:52:07 +0530

    initialising db

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const mongoURI =process.env.DB;
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected to Mongo Successfully");
+    })
+}
+
+module.exports = connectToMongo;
