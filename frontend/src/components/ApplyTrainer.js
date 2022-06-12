5afb38c79c5159aef0ff9a198b041844fb6ef0fc
Bhavin <bhavin1625p@gmail.com>
2022-06-12 06:37:47 +0530

    traine application proccess form and api end point done

--- frontend/src/components/ApplyTrainer.js
+++ frontend/src/components/ApplyTrainer.js
@@ -4,33 +4,36 @@ import './ApplyLoan.css'
 
 const ApplyTrainer = () => {
   return (
-    <div className="Demo container ">
-
-    <Form>
-        <h1>Document Verification for being a trainer</h1>
-        <div className="container d-flex flex-column justify-begin" >
-            <p>List of mandatory details to apply as a trainer -</p>
-
-            <li> Mobile Number </li>
-            <li> Aadhar card</li>
-            <li> CV </li>
-            <li> Sector and Couse Applied for</li>
-            <li> TOT Certificate - Yes - Pls upload </li>
-            <li> Marksheet </li>
-            <li> Gender</li>
-        </div>
-
-
-        <Form.Group controlId="formFileMultiple" className="mb-3">
-            <Form.Label className='d-flex align-start'> <b>Upload All your Documents</b> </Form.Label><br></br>
-            <Form.Control type="file" multiple />
+    <div className=" Demo container ">
+      <Form>
+        <h1>Apply as a trainer</h1>
+        <Form.Group className="mb-3" controlId="formGroupName" >
+          <Form.Label className='d-flex align-start'>Mobile Number</Form.Label><br></br>
+          <Form.Control name="name" type="string" placeholder="Enter Mobile Number" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label className='d-flex align-start'>Sector and Course Applied For: </Form.Label><br></br>
+          <Form.Control name="description" type="string" placeholder="Enter Category/Sector" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupAddress">
+          <Form.Label className='d-flex align-start'>Gender</Form.Label><br></br>
+          <Form.Control name="location" type="address" placeholder="Enter Gender" />
         </Form.Group>
 
+        <Form.Group controlId="formFileMultiple" className="mb-3">
+          <Form.Label className='d-flex align-start'> <b>Upload Following Documents:
+            Aadhar Card,
+            CV,
+            Marksheet,
+            TOT Certificate
+          </b> </Form.Label><br></br>
+          <Form.Control type="file" multiple />
+        </Form.Group>
 
-        <Button variant="warning">Send For Verification</Button>{' '}
+        <Button variant="warning">Apply for Good</Button>{' '}
 
-    </Form>
-</div>
+      </Form>
+    </div>
   )
 }
 
