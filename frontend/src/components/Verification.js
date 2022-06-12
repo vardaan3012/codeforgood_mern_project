8d6b9c60c9e97ac90b8a1c4e25642de7845ff4f3
Bhavin <bhavin1625p@gmail.com>
2022-06-12 08:15:02 +0530

    verification ui

--- frontend/src/components/Verification.js
+++ frontend/src/components/Verification.js
@@ -1,42 +1,93 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import './ApplyLoan.css'
 
-const data = [
-  { name: "Anom", age: 19, gender: "Male" },
-  { name: "Megha", age: 19, gender: "Female" },
-  { name: "Subham", age: 25, gender: "Male" },
-]
 
 function Disloan() {
+
+  const [data, setdata] = useState(null);
+
+  const fetchdata = () => {
+    fetch('http://localhost:8000/api/toverify', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token')
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setdata(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    fetchdata();
+  }, [])
+
+  const handleApprove = (e) => {
+    e.preventDefault();
+
+    fetch('http://localhost:8000/api/verify', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token')
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        fetchdata();
+        alert("Documents Verified!");
+      }
+      )
+      .catch(err => {
+        console.log(err);
+        alert("Documents Verified!");
+      }
+      );
+
+  }
+
+  const handleReject = (e) => {
+    e.preventDefault();
+    alert("Request is Rejected");
+  }
+
   return (
     <div className="">
       <h1 style={{ "marginTop": "20px" }}>Verification Requests </h1>
       <div className="Disloan container" style={{ "marginTop": "100px" }}>
-        <table>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Address</th>
-            <th>Adhaar</th>
-            <th>Pan Card</th>
-            <th>Approve</th>
-            <th>Reject</th>
-          </tr>
-          {data.map((val, key) => {
-            return (
-              <tr key={key}>
-                <td>Name</td>
-                <td>Email</td>
-                <td>Address</td>
-                <td>Adhaar</td>
-                <td>Address</td>
-                <td><Button variant="warning">Approve</Button></td>
-                <td><Button variant="warning">Reject</Button></td>
-              </tr>
-            )
-          })}
-        </table>
+
+        {data?.map((val, key) => {
+          return (
+            <div className="container" style={{
+              "backgroundColor": "#f5f5f5",
+            }}>
+              <ul key={key}>
+                <p><b>Name: </b>  {val.name}</p>
+                <p><b>Email: </b> {val.email}</p>
+                <p><b>Status: </b>  {val.isVerified ? "Verified" : "Not Verified"}</p>
+                <p><b>Documents Links: </b></p>
+                <p>{val.documents[0]}</p>
+                <p>{val?.documents[1]}</p>
+                <p>{val?.documents[2]}</p>
+                <div classname="d-flex">
+                  <Button onClick={handleApprove} variant="success">Approve</Button>
+                  <Button onClick={handleReject} variant="danger">Reject</Button>
+                </div>
+
+              </ul>
+            </div>
+          )
+        })}
+
       </div>
     </div>
 
